73ea95538a0aa298fa2d13a538b200d23a5fbc8e
gustavjung01 <gustavjung01@gmail.com>
2026-06-30 22:29:21 +0700

    Bump test delete fix import

--- src/data-admin-ui-owner.js
+++ src/data-admin-ui-owner.js
@@ -9,4 +9,4 @@ import './data-hub-shell.js?v=data-hub-order-manage-1';
 import './order-manage-actions.js?v=order-detail-1';
 import './order-detail-enhance.js?v=order-detail-1';
 import './order-data-hub-enhance.js?v=order-data-hub-1';
-import './test-report-manage-actions.js?v=test-report-manage-2';
+import './test-report-manage-actions.js?v=test-delete-fix-1';
